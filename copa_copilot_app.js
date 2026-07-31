92d6c98d96ca738710eeb262c22460202ce01d64
AndresArango <arangoandres20@gmail.com>
2026-07-31 10:23:47 -0500

    base1

--- copa_copilot_app.js
+++ copa_copilot_app.js
@@ -287,7 +287,7 @@ function renderTable() {
           <span class="pct-num">${medal} ${pct}%</span>
         </div>
       </td>
-      <td>$<span class="currency-cell">${formatCurrency(row.inter)}</span></td>
+      <td><span class="currency-cell">${formatCurrency(row.inter)}</span></td>
       <td><span class="currency-cell">${formatCurrency(row.goles)}</span></td>
       <td>${formatCurrency(row.compromiso)}</td>
       <td><span class="${row.falta < 0 ? 'falta-negativa' : 'falta-positiva'}">${formatCurrency(row.falta)}</span></td>
